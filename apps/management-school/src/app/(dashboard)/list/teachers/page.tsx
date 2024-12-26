3101e2b525a79d67c0f1913d866410e6c63bc0a4
Nailalvz <suareznaila@gmail.com>
2024-12-26 15:35:34 +0100

    add visible button only for admin role

--- apps/management-school/src/app/(dashboard)/list/teachers/page.tsx
+++ apps/management-school/src/app/(dashboard)/list/teachers/page.tsx
@@ -54,7 +54,10 @@ const columns = [
 
 const TeacherListPage = () => {
   const renderRow = (item: Teacher) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
       <td className="flex items-center gap-4 p-4">
         <img
           src={item.photo}
@@ -104,9 +107,11 @@ const TeacherListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <img src="/sort.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <img src="/plus.png" alt="" width={14} height={14} />
-            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                <img src="/plus.png" alt="" width={14} height={14} />
+              </button>
+            )}
           </div>
         </div>
       </div>
