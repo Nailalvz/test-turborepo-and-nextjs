1ab4c1368db0361845c64562fac2fc36dea1dc15
Nailalvz <suareznaila@gmail.com>
2024-12-26 14:08:22 +0100

    add role

--- apps/management-school/src/app/(dashboard)/layout.tsx
+++ apps/management-school/src/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Menu from "@repo/ui/menu";
 import Navbar from "@repo/ui/navbar";
+import { role } from "../lib/data";
 
 
 export default function DashboardLayout({
@@ -17,11 +18,11 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
           <span className="hidden lg:block font-bold">Schoolama</span>
         </Link>
-        <Menu />
+        <Menu role={role} />
       </div>
 
       {/* Right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
--- packages/ui/src/menu.tsx
+++ packages/ui/src/menu.tsx
@@ -113,18 +113,22 @@ const menuItems = [
   },
 ];
 
-const Menu = () => {
+const Menu = ({ role }: { role: string }) => {
   return (
     <div className="ui-mt-4 ui-text-sm">
       {menuItems.map((i) => (
         <div className="ui-flex ui-flex-col ui-gap-2 ui-px-4" key={i.title}>
           <span className="ui-hidden lg:ui-block ui-text-gray-400 ui-font-light ui-my-2">{i.title}</span>
-          {i.items.map((item) => (
-            <a className="ui-flex ui-items-center ui-justify-center lg:ui-justify-start ui-gap-4 ui-text-gray-500 ui-py-2" key={item.label} href={item.href}>
-              <img src={item.icon} alt={item.label} width={20} height={20}></img>
-              <span className="ui-hidden lg:ui-block">{item.label}</span>
-            </a>
-          ))}
+          {i.items.map((item) =>{ 
+          if(item.visible.includes(role)) {
+            return (
+              <a className="ui-flex ui-items-center ui-justify-center lg:ui-justify-start ui-gap-4 ui-text-gray-500 ui-py-2 md:ui-px-2 ui-rounded-md hover:ui-bg-lamaSkyLight" key={item.label} href={item.href}>
+                <img src={item.icon} alt={item.label} width={20} height={20}></img>
+                <span className="ui-hidden lg:ui-block">{item.label}</span>
+              </a>
+            )
+          }}
+          )}
         </div>
       ))}
     </div>
