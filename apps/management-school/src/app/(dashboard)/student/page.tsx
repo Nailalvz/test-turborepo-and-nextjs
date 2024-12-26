b4faa23dec0399b740764e29448cb8d71651bf14
Nailalvz <suareznaila@gmail.com>
2024-12-26 14:08:40 +0100

    add new component bigCalendar

--- apps/management-school/src/app/(dashboard)/student/page.tsx
+++ apps/management-school/src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,27 @@
+import Announcements from "@repo/ui/announcements";
+import EventCalendar from "@repo/ui/eventCalendar";
+import BigCalendar from "@repo/ui/bigCalendar";
+import { calendarEvents } from "@/app/lib/data";
+
+
 const StudentPage = () => {
-    return (
-      <div className=''>StudentPage</div>
-    )
-  }
-  
-  export default StudentPage;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar events={calendarEvents}/>
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default StudentPage;
