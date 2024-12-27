67f373c351335cfa4895019f5813b4891fa27e2b
Nailalvz <suareznaila@gmail.com>
2024-12-27 09:55:35 +0100

    add events list page

---
+++ apps/management-school/src/app/(dashboard)/list/events/page.tsx
@@ -0,0 +1,115 @@
+import TableSearch from "@repo/ui/tableSearch";
+import Pagination from "@repo/ui/pagination";
+import Table from "@repo/ui/table";
+import Link from "next/link";
+import { eventsData, role } from "@/app/lib/data";
+
+type Event = {
+  id: number;
+  title: string;
+  class: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+};
+
+const columns = [
+  {
+    header: "Title",
+    accessor: "title",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "End Time",
+    accessor: "endTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
+
+const EventsListPage = () => {
+  const renderRow = (item: Event) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.title}</h3>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden lg:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.startTime}</td>
+      <td className="hidden md:table-cell">{item.endTime}</td>
+      <td className="">
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <img src="/edit.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <img src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <img src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <img src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                <img src="/plus.png" alt="" width={14} height={14} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* LIST */}
+      <div className="">
+        <Table columns={columns} renderRow={renderRow} data={eventsData} />
+      </div>
+
+      {/* PAGINATION */}
+      <div className="">
+        <Pagination />
+      </div>
+    </div>
+  );
+};
+
+export default EventsListPage;
